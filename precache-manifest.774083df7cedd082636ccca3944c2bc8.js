e9d0dba4a8e87e745b567ea9d90dbe84024c8eca
Jirayr <jirayrgrigoryan1985@gmail.com>
2020-06-07 18:28:38 +0400

    Updates

---
+++ precache-manifest.774083df7cedd082636ccca3944c2bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4816a675eec22a2929d48b18c40101e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06c9fcd0a303d1ae79e0",
+    "url": "/monsters-rolodex/static/css/main.9c67c53f.chunk.css"
+  },
+  {
+    "revision": "55a3a531cf3aa04436a7",
+    "url": "/monsters-rolodex/static/js/2.f71b2f1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f71b2f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c9fcd0a303d1ae79e0",
+    "url": "/monsters-rolodex/static/js/main.81f336c1.chunk.js"
+  },
+  {
+    "revision": "12cbc2b24df4a07ef08e",
+    "url": "/monsters-rolodex/static/js/runtime-main.d4fbbfac.js"
+  }
+]);
